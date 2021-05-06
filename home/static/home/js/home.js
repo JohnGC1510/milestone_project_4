00ea8e99e76f0885cffdd91e8073e28901e8d775
JohnGC1510 <johngc1510@gmail.com>
2021-05-06 16:27:37 +0000

    Added template of product carosuel to home page

---
+++ home/static/home/js/home.js
@@ -0,0 +1,24 @@
+/* Setup for product Carosuel came from
+https://stackoverflow.com/questions/20007610/bootstrap-carousel-multiple-frames-at-once*/
+$('#product-carousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
