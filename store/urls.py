c2e9052495de10edc37818625ad3a23b7704a44b
JohnGC1510 <johngc1510@gmail.com>
2021-05-12 09:42:56 +0000

    This version has a basic tempalte, view and url for product details and is able to perform search queires on the store page

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.store_home, name="store"),
+    path('<product_id>', views.product_detail, name="product_detail")
 ]
--- store/views.py
+++ store/views.py
@@ -1,15 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def store_home(request):
     """ A view to return the home page"""
     template = 'store/store_home.html'
+    query = None
 
     products = Product.objects.all()
 
-    context ={
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
         'products': products,
+        'search_term': query,
+    }
+
+    return render(request, template, context)
+
+
+def product_detail(request, product_id):
+    """ A view to show the individial product details """
+    template = 'store/product_detail.html'
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context ={
+        'product': product,
     }
 
     return render(request, template, context)
