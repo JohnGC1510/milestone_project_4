9fdfcc9d39267348e8d9881d0e405bebe48cac35
JohnGC1510 <johngc1510@gmail.com>
2021-05-17 08:24:54 +0000

    This version has a contexts.py in the bag app and the basic structure of the bag template has been created

--- golds_gym/settings.py
+++ golds_gym/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -190,6 +191,9 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+FREE_DELIVERY_THRESHOLD = 500
+STANDARD_DELIVERY_PERCENTAGE = 5
 """
 # stripe
 FREE_DELIVERY_THRESHOLD = 50
