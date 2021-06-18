61dc3be9924e3ff746ae708979754233d0f9ec23
JohnGC1510 <johngc1510@gmail.com>
2021-06-18 15:05:08 +0000

    This version contains class methods to webhook handler and webhook view

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,5 +10,17 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """ Handle a generic/unknown webhook """
         return HttpResponse(
+            content=f'Unhandled Webhook recieved: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """ Handle a webhook for a successful payment intent """
+        return HttpResponse(
+            content=f'Webhook recieved: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ Handle a webhook for a failed payment intent """
+        return HttpResponse(
             content=f'Webhook recieved: {event["type"]}',
             status=200)
--- golds_gym/settings.py
+++ golds_gym/settings.py
@@ -211,9 +211,8 @@ STANDARD_DELIVERY_PERCENTAGE = 5
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 """
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
-
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'goldsgym@example.com'
