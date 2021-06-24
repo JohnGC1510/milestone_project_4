ede8eb8fc16ebf34084583f54ca6d70a33d35ea6
JohnGC1510 <johngc1510@gmail.com>
2021-06-24 14:02:49 +0000

    Added the functionallity for superusers to edit products

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.store_home, name="store"),
     path('<int:product_id>/', views.product_detail, name="product_detail"),
     path('add_product/', views.add_product, name="add_product"),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
